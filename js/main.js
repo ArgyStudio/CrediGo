acc09ea1c5b5b1a23f7b831b29bc4ab44982e1cd
MALEGO <argystudio@gmail.com>
2026-01-06 12:31:59 -0300

    Initial commit: Landing Page CrediGo

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+document.getElementById("button-cta").addEventListener('click', () => {
+    console.log("click")
+
+    //  --display--
+    const overlay = document.getElementById("black-overlay")
+    // main.classList.add("hidden")
+    overlay.classList.add("active")
+
+    // --wsp--
+    const lines = ["5493815898254", "5493815898297"]
+    const lineR = lines[Math.floor(Math.random() * lines.length)]
+    const msg = "Hola CrediGo! Quiero mas información..."
+    const URL = `https://wa.me/${lineR}?text=${encodeURIComponent(msg)}`
+    window.location.href = URL
+})
