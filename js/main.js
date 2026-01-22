eda434ca5257190d88f91849fdfeb905bdd62b30
MALEGO <argystudio@gmail.com>
2026-01-22 15:33:28 -0300

    modf : lines

--- js/main.js
+++ js/main.js
@@ -10,14 +10,11 @@ document.getElementById("button-cta").addEventListener('click', () => {
 
     // --wsp--
     const lines = [
-        "5491125050882",
         "5491125050972",
         "5491124973143",
         "5491162430169",
-        "5491125052261",
         "5491164567789",
         "5491125052196",
-        "5491125050729"
     ]
     const lineR = lines[Math.floor(Math.random() * lines.length)]
     const msg = "Hola CrediGo! Quiero mas información..."
