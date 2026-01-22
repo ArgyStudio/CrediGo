2260b629c89f586b9fd7467e664bf60a77b152e3
MALEGO <argystudio@gmail.com>
2026-01-22 09:18:18 -0300

    modf : lines

--- js/main.js
+++ js/main.js
@@ -9,7 +9,16 @@ document.getElementById("button-cta").addEventListener('click', () => {
     overlay.classList.add("active")
 
     // --wsp--
-    const lines = ["5491162696275", "5491168500281", "5491164567789"]
+    const lines = [
+        "5491125050882",
+        "5491125050972",
+        "5491124973143",
+        "5491162430169",
+        "5491125052261",
+        "5491164567789",
+        "5491125052196",
+        "5491125050729"
+    ]
     const lineR = lines[Math.floor(Math.random() * lines.length)]
     const msg = "Hola CrediGo! Quiero mas información..."
     const URL = `https://wa.me/${lineR}?text=${encodeURIComponent(msg)}`
